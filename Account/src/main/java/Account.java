ad29a281ee1f939645cca8348b798e09970683ee
AlexeyK1212 <161300105+AlexeyK1212@users.noreply.github.com>
2024-02-27 14:10:18 +0300

    Add files via upload

--- Account/src/main/java/Account.java
+++ Account/src/main/java/Account.java
@@ -4,12 +4,20 @@ import java.util.HashMap;
 import java.util.Stack;
 
 //для выполнения команд по отмене  действий над аккаунтом
-abstract class Command {
+interface Command {
     public abstract void undo();
 }
 
 
-
+enum Currencys {
+    RUR(810),
+    USD(840),
+    EUR(978);
+    public final int i;
+    Currencys(int i) {
+        this.i=i;
+    }
+}
 
 
 
@@ -17,7 +25,7 @@ abstract class Command {
 
 // Класс аккаунта
 public class Account {
-    private String Name;
+    private String name;
     private HashMap<Integer, Integer> currencyAccounts;
     private Stack<Command> history;
 
@@ -26,12 +34,21 @@ public class Account {
 
 
 
-    public Account(String Name) {
-        this.Name = Name;
+    public Account(String name) throws undoException {
+        if(name.isEmpty()) {
+            throw new undoException("Название счета не может быть пустым");
+        }
+        if(name=="") {
+            throw new undoException("Название счета не может быть пустым");
+        }
+
+        this.name = name;
         this.currencyAccounts = new HashMap<>();
-        currencyAccounts.put(810,0);
-        currencyAccounts.put(840,0);
-        currencyAccounts.put(978,0);
+
+        for (Currencys curr : Currencys.values())
+            currencyAccounts.put(curr.i,0);
+
+
         this.history = new Stack<>();
     }
 
@@ -44,19 +61,32 @@ public class Account {
     }
 
     // метод изменения строки
-    public void updateName(String newValue) {
-        final String oldValue = Name;
-        Name = newValue;
+    public void updateName(String newValue) throws undoException {
+
+        if(newValue.isEmpty()) {
+            throw new undoException("Название счета не может быть пустым");
+        }
+        if(newValue=="") {
+            throw new undoException("Название счета не может быть пустым");
+        }
+
+        final String oldValue = name;
+        name = newValue;
         history.push(new Command() {
             @Override
             public void undo() {
-                Name = oldValue;
+                name = oldValue;
             }
         });
     }
 
     // метода изменения HashMap
-    public void updateCurrencyAccounts(Integer key, Integer value) {
+    public void updateCurrencyAccounts(Integer key, Integer value) throws undoException {
+
+        if(key<=0) {
+            throw new undoException("Код валюты должен быть больше нуля");
+        }
+
         final Integer oldValue = currencyAccounts.get(key);
         currencyAccounts.put(key, value);
         history.push(new Command() {
@@ -73,11 +103,15 @@ public class Account {
 
     // Геттеры для полей (для проверки состояния)
     public String getName() {
-        return Name;
+        return name;
     }
 
     public HashMap<Integer, Integer> getCurrencyAccounts() {
-        return currencyAccounts;
+        return new HashMap<Integer, Integer>(currencyAccounts);
+    }
+
+    public int getValueByCurrencyCode(int CurrencyCode) {
+        return currencyAccounts.get(CurrencyCode);
     }
 
 
@@ -85,7 +119,7 @@ public class Account {
 
     public AccountMemento save() {
 
-        return new AccountMemento(Name, currencyAccounts);
+        return new AccountMemento(name, currencyAccounts);
 
     }
 
@@ -94,9 +128,48 @@ public class Account {
 
     public void restore(AccountMemento memento) {
 
-        this.Name = memento.getStringFieldState();
+        this.name = memento.getStringFieldState();
 
         this.currencyAccounts = new HashMap<>(memento.getHashMapFieldState());
+
+    }
+
+
+    public class AccountMemento {
+
+        private final String stringFieldState;
+
+        private final HashMap<Integer, Integer> hashMapFieldState;
+
+
+
+        public AccountMemento(String stringField, HashMap<Integer, Integer> hashMapField) {
+
+            this.stringFieldState = stringField;
+
+            this.hashMapFieldState = new HashMap<>(hashMapField);
+
+        }
+
+
+
+        // Получаем состояние stringField
+
+        public String getStringFieldState() {
+
+            return stringFieldState;
+
+        }
+
+
+
+        // Получаем состояние hashMapField
+
+        public HashMap<Integer, Integer> getHashMapFieldState() {
+
+            return hashMapFieldState;
+
+        }
 
     }
 
